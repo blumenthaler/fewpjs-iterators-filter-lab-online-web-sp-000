a176777c6eb3bb9fb81b3b1e13ef03e79cae6f92
blumenthaler <blumenthaleralex@gmail.com>
2020-11-17 16:07:18 -0500

    commit tree cdc4f6bba3d47cc5c352c7e0918732650929a006

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function (driver) {
+      return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (driver) {
+        return ((driver.charAt(0) + driver.charAt(1)) === (string.charAt(0) + string.charAt(1)))
+    })
+}
+
+let test = [{name: "Jim"}, {name: "Alex"}, {name: "Leo"}];
+
+// The function should return each element whose name property matches the provided string argument.
+
+function matchName(array, string) {
+    return array.filter(function (object) {
+        return object.name === string
+    })
+}
+
+
